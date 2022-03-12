85a17655a4e2f0de107d94303a0422333400c328
harsha vardhan <98849090+Harsha7337@users.noreply.github.com>
2022-03-12 11:34:07 +0530

    Update driver.h

--- PROJECT1/3_Implementation/driver.h
+++ PROJECT1/3_Implementation/driver.h
@@ -5,14 +5,14 @@
 
 #define BTN_PRESSED ENABLE
 // enabaling the button press
-void windowstatus();
-// shows window status when function is called
-void alarmststus();
-// shows alarmstatus when the function is called
-void batteryinfo();
-// shows battery info when the function isa called
-void doorstatus();
-// shows door status when the function is called
+void lock();
+// shows lock when function is called
+void unlock();
+// shows unlock when the function is called
+void alarm ON/OFF ();
+// shows alarm ON/OFF when the function isa called
+void Car approach ON();
+// shows car approach ON when the function is called
 void delay_1(void);
 // provides required delay
 int button_count(void);       // Providing Count Value
