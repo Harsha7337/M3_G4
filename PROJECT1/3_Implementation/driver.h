86db9a074c529fafe01686cc0a38891efb717716
harsha vardhan <98849090+Harsha7337@users.noreply.github.com>
2022-03-12 08:46:50 +0530

    Create driver.h

---
+++ PROJECT1/3_Implementation/driver.h
@@ -0,0 +1,20 @@
+#include "MyStm32f407xx.h"
+#include<stdio.h>
+#define "MAX 5"
+#ifndef "MAX 5"
+
+#define BTN_PRESSED ENABLE
+// enabaling the button press
+void windowstatus();
+// shows window status when function is called
+void alarmststus();
+// shows alarmstatus when the function is called
+void batteryinfo();
+// shows battery info when the function isa called
+void doorstatus();
+// shows door status when the function is called
+void delay_1(void);
+// provides required delay
+int button_count(void);       // Providing Count Value
+
+#endif
